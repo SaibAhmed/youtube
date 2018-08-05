ccc13afac487a68d468b55e41b85a2bea4db91e2
Saib Ahmed <saibahmed1512@gmail.com>
2018-08-05 12:14:20 +0530

    commit

--- src/Youtube.js
+++ src/Youtube.js
@@ -5,16 +5,45 @@ const Results=10;
 
 var finalUrl=`https://www.googleapis.com/youtube/v3/search?key=${Api}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${Results}`
 class Youtube extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      resultYt:[]
+    };
+    this.clicked=this.clicked.bind(this);
+
+  }
+  clicked(){
+    fetch(finalUrl)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      // console.log(responseJson);
+
+      const resultYt = responseJson.items.map(obj =>"https://www.youtube.com/embed/"+obj.id.videoId);
+      this.setState({resultYt});
+
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
   render(){
     console.log(finalUrl);
+    console.log(this.state.resultYt);
     return(
       <div>
-        <button>Get youtube videos</button>
+        <button onClick={this.clicked}>Get youtube videos</button>
+          {
+            this.state.resultYt.map((link,i) =>{
+            var frame =<div key={i} className="youtube"><iframe  width="560" height="315" src={link} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></div>
+             return frame;
+           })
+          }
+          {this.frame}
 
-      <div className="youtube">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/OMvpBHtmrJo" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
-      </div>
-      </div>
+        </div>
     );
   }
 }
